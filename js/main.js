a7e9e889734c19e19b5629ccda7b124d4a2b423f
chirayurathi <chirayurathi4@gmail.com>
2021-07-16 18:58:14 +0530

    navbar

--- js/main.js
+++ js/main.js
@@ -92,28 +92,46 @@
 	/*-----------------------------------------------------*/
   	/* Navigation Menu
    ------------------------------------------------------ */
-   var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
+//    var toggleButton = $('.menu-toggle'),
+//        nav = $('.main-navigation');
 
-   // toggle button
-   toggleButton.on('click', function(e) {
+//    // toggle button
+//    toggleButton.on('click', function(e) {
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
+// 		e.preventDefault();
+// 		toggleButton.toggleClass('is-clicked');
+// 		nav.slideToggle();
 
-	});
+// 	});
 
-   // nav items
-  	nav.find('li a').on("click", function() {
+//    // nav items
+//   	nav.find('li a').on("click", function() {
 
-   	// update the toggle button
-   	toggleButton.toggleClass('is-clicked');
-   	// fadeout the navigation panel
-   	nav.fadeOut();
+//    	// update the toggle button
+//    	toggleButton.toggleClass('is-clicked');
+//    	// fadeout the navigation panel
+//    	nav.fadeOut();
 
-  	});
+//   	});
 
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('nav ul')
+    const navLink = document.querySelectorAll('nav ul li')
+    burger.onclick = ()=>{
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('active');
+        navLink.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else{
+                link.style.animation = `navFade 0.5s ease forwards ${index/7 + 0.5}s`
+            }
+        })
+    }
+}
+navSlide();
 
    /*---------------------------------------------------- */
   	/* Highlight the current section in the navigation bar
